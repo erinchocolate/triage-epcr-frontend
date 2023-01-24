2c02932859ee47b2d4608d96b0ea42d864d99263
Joshua Lee <joshleecloud@gmail.com>
2023-01-25 11:32:31 +1300

    Josh's First Commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Josh is the laddiest </Text>
       <StatusBar style="auto" />
     </View>
   );
