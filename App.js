e493f55f239ff1861a3dd2f86fbd794a8a75861c
Joshua Lee <joshleecloud@gmail.com>
2023-01-25 15:02:12 +1300

    Prototype of top menu

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar  as ExpoStatus} from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, StatusBar, Platform } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
 import Header from './components/Header';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
       <Text style={styles.joshtext}>Josh is the best</Text>
-      <ExpoStatus style="auto" />
+      <ExpoStatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
     paddingTop: StatusBar.currentHeight
   },
   joshtext: {
--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,59 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, TouchableOpacity  } from 'react-native';
 
 export default function Header(){
     return(
         <View style={styles.layout}>
-            <Button title='Button 1' style={styles.button} />
-            <Button title='Button 2' />
-            <Button title='Button 3' />
-            <Button title='Button 4' />
-            <Button title='Button 5'/>
-            <Button title='Button 6'/>
-            <Button title='Button 7'/>
-            <Button title='Button 8'/>
-            <Button title='Button 9'/>
-            <Button title='Button 10'/>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>B</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>C</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>D</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>E</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>F</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>G</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>H</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>I</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.item}>
+                <Text style={styles.myText}>J</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
-const styles = ()=> StyleSheet.create({
+const styles = StyleSheet.create({
     layout:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
         flexDirection: 'row',
-        flexWrap: 'wrap'
+        height: '40%',
+        width: '100%',
+        backgroundColor: 'dodgerblue'
     },
-    button:{
+    item:{
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        height: '25%',
+        width: '20%'
     }
 })
 
