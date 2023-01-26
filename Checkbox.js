b251b886b22d4ac0613bbdeaaf9d1181f726c7a2
dazzikay <darina.khun@gmail.com>
2023-01-26 14:10:15 +1300

    A file for a single checkbox

---
+++ Checkbox.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { useState } from 'react';
+
+export default function Intervention() { 
+    const [isChecked, setChecked] = useState(false);
+
+    return(
+    <View style={styles.container}>
+         <Checkbox 
+            style={styles.checkbox}
+            color={isChecked? "#00FF00": undefined}
+            value={isChecked} onValueChange={setChecked} 
+         />
+        <Text>Option</Text>
+
+    </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flex:1,
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#4BAFEB',
+        alignItems: 'center',
+        justifyContent: 'center',
+    
+    },
+
+    checkbox: {
+        margin: 8
+    },
+  });
