e5db7d235a628df31fa3ce2865a30a1335273afd
Joshua Lee <joshleecloud@gmail.com>
2023-01-26 14:56:53 +1300

    Testing with Dropdown

--- components/IncidentDetails.js
+++ components/IncidentDetails.js
@@ -1,27 +1,20 @@
 import { StyleSheet, Text, View, FlatList, Dimensions, TouchableOpacity  } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { useState } from 'react';
-import Checkbox from '../utility/DropDown';
+import DropDown from '../utility/DropDown';
 
 export default function IncidentDetails() {
-
-    const [open, setOpen] = useState(false);
-    const[value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        {label: 'Option 1', value: 'O1'},
-        {label: 'Option 2', value: 'O2'},
-        {label: 'Option 3', value: 'O3'}
-    ]);
-
-    
- 
+     
     return (
     <View style={styles.layout}>
         <Text>Wow, look! A Dropbox!</Text>
-        <Checkbox 
-                placeholder = 'MyPlaceholder'
-                items = {[{label: 'Option 1', value: '1'},
-                {label: 'Option 2', value: '2'}]}/>
+        <DropDown
+                placeholder = 'Josh'
+                items = {[
+                    {label: 'Darina', value: 'D'},
+                    {label: 'Harry', value: 'H'},
+                    {label: 'Meiqiao', value: 'M'}
+                ]} />
     </View>
   )
 }
