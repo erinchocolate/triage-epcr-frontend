b5097bf0f4584173f2beaac7b5255fe0d15dffad
dazzikay <darina.khun@gmail.com>
2023-01-28 14:05:54 +1300

    Checkbox,textbox,dropdown box all in intervention

--- Checkbox.js
+++ Checkbox.js
@@ -2,7 +2,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { useState } from 'react';
 
-export default function Checkboxes() { 
+
+export default function CheckBox() { 
     const [isChecked, setChecked] = useState(false);
 
     return(
@@ -12,7 +13,7 @@ export default function Checkboxes() {
             color={isChecked? "#00FF00": undefined}
             value={isChecked} onValueChange={setChecked} 
          />
-        <Text>Option</Text>
+      
 
     </View>
     );
@@ -20,17 +21,6 @@ export default function Checkboxes() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        flex:1,
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#4BAFEB',
-        alignItems: 'center',
-        justifyContent: 'center',
-    
-    },
 
     checkbox: {
         margin: 8
--- Intervention.js
+++ Intervention.js
@@ -1,14 +1,41 @@
 
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { useState } from 'react';
 import DropDown from './DropDown';
+import React from 'react';
+import CheckBox from './Checkbox';
 
-  export default function Intervention() { 
 
-        return (
+export default function Intervention() { 
 
-          <View style={styles.container}>
-          <View style={styles.smallDropdown}> 
+    //this is used for the state of the keypad
+    const [text, onChangeText] = useState('');
+
+ return (
+
+  //main box that everything fits into
+  <View style={styles.component}>  
+
+  
+    <View style={styles.container}>
+
+        <CheckBox/>
+        <Text>OPA</Text>
+
+        <CheckBox/>
+        <Text>LMA</Text>
+
+        <CheckBox/>
+        <Text>NPA</Text>
+
+        <CheckBox/>
+        <Text>ETT</Text>
+
+    </View>
+
+
+    <View style={styles.container}>
+      <View style={styles.smallDropdown}> 
            <DropDown
             placeholder = 'Size'
             items = {[
@@ -25,11 +52,11 @@ import DropDown from './DropDown';
               {label: '8', value: '8'},
               {label: '9', value: '9'}
             ]}
-           />
-           </View>
+            />
+      </View>
 
 
-          <View style={styles.largeDropdown}> 
+      <View style={styles.largeDropdown}> 
           <DropDown
             placeholder = 'Location'
             items = {[
@@ -38,41 +65,146 @@ import DropDown from './DropDown';
               
             ]}
            />
-          </View>
+      </View>
+    
+    </View>
+
+
+    <View style={styles.container}>
+
+          <CheckBox/>
+          <Text>PEEP</Text>
+
+          <CheckBox/>
+          <Text>Suction</Text>
+
+          <CheckBox/>
+          <Text>BVM</Text>
+
+    </View>
+
+
+    <View style={styles.container}>
+
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={text}
+            placeholder="Bring up a keypad"
+          />
+
+      <View style={styles.largeDropdown}> 
+          <DropDown
+              placeholder = 'Suction Catheter'
+              items = {[
+              {label: 'Opt 1', value: 'Opt 1'},
+              {label: 'Opt 2', value: 'Opt 2'},
+      
+              ]}
+            />
+        </View>
+
+    </View>
+
+    <View style={styles.container}>
+
+          <CheckBox/>
+          <Text>IO</Text>
+
+            <CheckBox/>
+            <Text>IV</Text>
+
+    </View>
 
 
 
-          
-          </View>
+    <View style={styles.container}>
+
+      <View style={styles.smallDropdown}> 
+           <DropDown
+            placeholder = 'Size'
+            items = {[
+              {label: '0', value: '0'},
+              {label: '00', value: '00'},
+              {label: '000', value: '000'},
+            
+            ]}
+           />
+      </View>
 
 
+      <View style={styles.largeDropdown}> 
+          <DropDown
+            placeholder = 'Location'
+            items = {[
+              {label: 'Opt 1', value: 'Opt 1'},
+              {label: 'Opt 2', value: 'Opt 2'},
+              
+            ]}
+           />
+       </View>
+
+          <TouchableOpacity style={styles.button}>
+                  <Text style={styles.text}>Add</Text>
+          </TouchableOpacity>     
+
+    </View>
+
+    
 
 
-        );
+</View>
+
+
+    );
     
   }
 
   const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        flex:1,
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#4BAFEB',
-        alignItems: 'center',
-        justifyContent: 'center',
+    component: {
+      backgroundColor: "dodgerblue",
+      flex:1,
+      flexDirection: "column",
+      justifyContent: "space-around",
+    },
     
+    
+    container: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
     },
 
 
-    
+  
     smallDropdown: {
-      width: '40%'
+      width: '20%'
+      
+
     },
   
     largeDropdown: {
-      width: '60%'
+      width: '40%'
+    },
+
+    button: {
+      backgroundColor: "#EBEBEB",
+      borderRadius: 10,
+      width: 70,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+ 
+    },
+
+    input: {
+      height: 40,
+      width:'40%',
+      borderWidth: 1,
+      backgroundColor: "white",
+      margin: 12,
+      padding: 10,
+      
     },
 
 
