39ea2c1b9edb8a82cc1ea5bf794a8ac4b6d884bb
Joshua Lee <joshleecloud@gmail.com>
2023-01-31 14:09:26 +1300

    Fixed Medication error

--- components/Medications.js
+++ components/Medications.js
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
         marginTop: 'auto'  
     },
     medicationText:{
-        fontSize: '30%'
+        fontSize: 30
     },
     deleteButton:{
         ...commonStyle,
